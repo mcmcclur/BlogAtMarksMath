b239f5589a1d2ad97aeed5b27aba6e165655894e
Mark McClure <mcmcclur@unca.edu>
2025-07-16 16:38:33 -0400

    Setting style for scrollytelling in The Woods

---
+++ posts/TheWoods/components/set_scrollama.js
@@ -0,0 +1,55 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import scrollama from 'https://cdn.jsdelivr.net/npm/scrollama@3.2.0/+esm';
+
+export function set_scrollama(sticky_viz) {
+  const scroll_container = d3
+    .select("section#scroll_container");
+  const figure = scroll_container.select("figure");
+  const article = scroll_container.select("article");
+  const step0 = article.select('.step')
+  const step = article.selectAll(".step");
+  // const info = figure.select('.info')
+
+  const scroller = scrollama();
+  function handleResize() {
+    const stepH = Math.floor(window.innerHeight * 0.8);
+    const figureHeight = window.innerHeight*0.95;
+    const figureMarginTop = (window.innerHeight - figureHeight)/2;
+    figure
+      .style("height", figureHeight + "px")
+      .style("top", figureMarginTop + "px");
+    step.style("height", stepH + "px");
+    step0
+      // .style("height", '200px')
+      .style("margin-top", `-${figureHeight-100}px`)
+    scroller.resize();
+  }
+
+  function handleStepEnter({element, index, direction}) {
+    // const sv = d3.select(sticky_viz);
+    d3.select(element).style('background-color', 'rgba(255, 0, 0, 0.2)');
+  }
+ function handleStepExit({element, index, direction}) {
+    // const sv = d3.select(sticky_viz);
+    d3.select(element).style('background-color', 'rgba(255, 255');
+  }
+
+
+  function init() {
+    handleResize();
+    scroller
+      .setup({
+        step: "#scroll_container article .step",
+        offset: 0.6,
+        debug: false
+      })
+      .onStepEnter(handleStepEnter)
+      .onStepExit(handleStepExit);
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  // kick things off
+  init();
+}
