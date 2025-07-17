df676d869f62b34bd0c5a9213504d31555955c51
Mark McClure <mcmcclur@unca.edu>
2025-07-17 11:30:05 -0400

    Move The Woods back to just map, no scrolly telling but the map is enahanced.

--- posts/TheWoods/components/make_map.js
+++ posts/TheWoods/components/make_map.js
@@ -8,14 +8,15 @@ mapboxgl.accessToken = MAPBOX_TOKEN;
 ////////////////////////////
 // The main function.
 
-export function make_map(construction) {
+export function make_map(map_data) {
+  const construction = map_data.Construction
   const map = new mapboxgl.Map({
     container: 'map',
-    zoom: 15.3,
-    center: [-82.563, 35.6115],
+    zoom: 13.5,
+    center: [-82.57, 35.617],
     // style: "mapbox://styles/mapbox/outdoors-v12"
-    style: "mapbox://styles/mapbox/standard-satellite"
-    // style: "mapbox://styles/mapbox/standard"
+    // style: "mapbox://styles/mapbox/standard-satellite"
+    style: "mapbox://styles/mapbox/standard"
     // style: "mapbox://styles/mapbox/light-v11"
   });
 
@@ -25,12 +26,42 @@ export function make_map(construction) {
       new mapboxgl.NavigationControl({ showCompass: false }),
       "top-right"
     );
+    add_unca_layers();
     map.set_style = set_style;
-    map.show_construction = true;
-    set_style("mapbox://styles/mapbox/standard-satellite");
+    map.show_construction = false;
+    // set_style("mapbox://styles/mapbox/standard-satellite");
+
   });
 
   return map;
+
+  function add_unca_layers() {
+     Object.keys(map_data).slice(1).forEach(function (key) {
+      map.addSource(key, {
+        type: "geojson",
+        data: map_data[key]
+      });
+      map.addLayer({
+        id: key,
+        type: "fill",
+        source: key,
+        paint: {
+          "fill-color": "#aaa",
+          "fill-opacity": 0.2
+        }
+      });
+      map.addLayer({
+        id: key + "Boundary",
+        type: "line",
+        source: key,
+        paint: {
+          "stroke-color": "#666",
+          "stroke-width": 2,
+          "stroke-opacity": 0.8
+        }
+      });
+    })
+  }
 
   function add_construction() {
     if (map.getLayer("constructionFill")) {
@@ -71,6 +102,7 @@ export function make_map(construction) {
     map.setStyle(style);
     map.once("styledata", function() {
       if (map.show_construction) add_construction();
+      add_unca_layers();
     });
     // map.once("styledata", add_construction);
   }
